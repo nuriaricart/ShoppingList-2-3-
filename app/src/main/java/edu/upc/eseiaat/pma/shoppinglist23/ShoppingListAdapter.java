c2801577ff7712c7e6ef32d6d27c852ccf9841e3
Nuria.ricart <nuria.rs96@gmail.com>
2017-10-20 15:52:25 +0200

    3) Implementació CheckBox al ShoppingListAdapter

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist23/ShoppingListAdapter.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist23/ShoppingListAdapter.java
@@ -1,9 +1,34 @@
 package edu.upc.eseiaat.pma.shoppinglist23;
 
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 
-public class ShoppingListAdapter extends ArrayAdapter {
+import java.util.List;
 
-    
+public class ShoppingListAdapter extends ArrayAdapter<String> {
 
+    public ShoppingListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View result = convertView;
+        if (result == null){
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            result = inflater.inflate(R.layout.shopping_item, null);
+        }
+        CheckBox checkBox = (CheckBox) result.findViewById(R.id.shopping_item);
+        String item_text = getItem(position);
+        checkBox.setText (item_text);
+        return result;
+    }
 }
