d2b784cf3270185071264b5e19531038616e9752
Nuria.ricart <nuria.rs96@gmail.com>
2017-10-20 16:13:08 +0200

    4) Classe ShoppingItem creada

---
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist23/ShoppingItem.java
@@ -0,0 +1,27 @@
+package edu.upc.eseiaat.pma.shoppinglist23;
+
+public class ShoppingItem {
+    private String text;
+    private boolean checked;
+
+    public ShoppingItem(String text, boolean checked) {
+        this.text = text;
+        this.checked = checked;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+}
